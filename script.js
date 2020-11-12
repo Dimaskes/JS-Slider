0eec69334a67d2ae42f5382cc36de8bdf94ab629
Dimaskes <d.imon-million@yandex.ru>
2020-11-12 16:26:21 +0400

    complete

---
+++ script.js
@@ -0,0 +1,46 @@
+const container = document.querySelector('.slider-container');
+const track = document.querySelector('.slider-track');
+const items = document.querySelectorAll('.slider-item');
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+const itemsImages = document.querySelectorAll('.slider-item--img');
+
+let position = 0;
+const slidesToShow = 3;
+const slidesToScroll = 1;
+
+const itemWidth = container.clientWidth / slidesToShow;
+const movePosition = slidesToScroll * itemWidth;
+const itemsCount = items.length;
+
+items.forEach((item) => {
+    item.style.minWidth = `${itemWidth}px`;
+})
+itemsImages.forEach((item) => {
+    item.style.width = `${itemWidth}px`;
+})
+
+
+btnNext.addEventListener('click', () => {
+    const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+    position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+    setPosition();
+    checkButtons();
+})
+
+btnPrev.addEventListener('click', () => {
+    const itemsLeft = Math.abs(position) / itemWidth;
+    position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+    setPosition();
+    checkButtons();
+})
+
+const setPosition = () => {
+    track.style.transform = `translateX(${position}px)`;
+}
+const checkButtons = () => {
+    btnPrev.disabled = position === 0;
+    btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+}
+
+checkButtons();
